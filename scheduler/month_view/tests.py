121f3bbde2aaff65a393e4f319927f7a87d69ee5
nplatte <77999574+nplatte@users.noreply.github.com>
2022-01-21 18:34:27 -0600

    Added first test to month_view

--- scheduler/month_view/tests.py
+++ scheduler/month_view/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+import month_view.views as views
 
-# Create your tests here.
+class TestLoginPage(TestCase):
+    # the login page needs to be the first page people see
+    # it needs to use the right template
+    # redirect good users to month view
+    # redirect bad users to login page
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_login_uses_right_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'login.html')
+
+    def test_login_redirects_to_month_view_on_success(self):
+        pass
+
+    def test_login_redirects_to_login_on_fail(self):
+        pass
