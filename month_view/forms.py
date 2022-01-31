c51aeb22231e35fd0d93e5f8803913dd88cfdeb4
nplatte <77999574+nplatte@users.noreply.github.com>
2022-01-31 10:57:28 -0600

    Removed old form code

--- month_view/forms.py
+++ month_view/forms.py
@@ -5,30 +5,6 @@ from django.forms import ModelForm
 from .models import Event
 
 
-COLORS = (
-            ('red', 'red'),
-            ('yellow', 'yellow'),
-            ('green', 'green')
-        )
-
-'''class EventForm(forms.Form):
-    event_name = forms.CharField(
-        widget=forms.fields.TextInput(attrs={
-            'class': 'event_name'
-        })
-        )
-    event_color = forms.ChoiceField(
-        choices = COLORS,
-        widget=forms.Select(attrs={
-            'class': 'event_color'
-        })
-    )
-    event_time = forms.TimeField(
-        widget=forms.TimeInput(attrs={
-            'class': 'event_time'
-        })
-    )'''
-
 class EventForm(ModelForm):
     class Meta:
         model = Event
