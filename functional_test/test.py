3eb75ee9d1fb5f2d278e6eae8534faeee661efd4
nplatte <77999574+nplatte@users.noreply.github.com>
2022-01-31 17:19:39 -0600

    Added a helper function to do login attempts

--- functional_test/test.py
+++ functional_test/test.py
@@ -27,27 +27,49 @@ class UserMakesEvent(StaticLiveServerTestCase):
         # Finally Tiddlywinks logs off the website
         self.browser.quit()
 
-    def _get_username_and_password_inputs(self):
-        username = self.browser.find_element_by_id('username')
-        password = self.browser.find_element_by_id('password')
-        submit = self.browser.find_element_by_id('login_button')
-        return username, password, submit
+    def _login_attempt(self, username, password):
+        username_input = self.browser.find_element_by_id('username')
+        password_input = self.browser.find_element_by_id('password')
+        login_button = self.browser.find_element_by_id('login_button')
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        login_button.click()
+        
+
+    def test_tiddlywinks_can_log_in_and_out(self):
+        # Tiddlywinks is greeted by a log in screen
+        self.assertIn('Log In', self.browser.title)
+        # They enter their username and password
+        self._login_attempt(self.test_username, 'assword123')
+        '''username_input, password_input, login_button = self._get_username_and_password_inputs()
+        username_input.send_keys(self.test_username)
+        password_input.send_keys('assword123')
+        login_button.click()'''
+        # Oh no! They entered it wrong and got prompted to reenter the information
+        self.assertNotIn('Scheduler', self.browser.title)
+        self.assertIn('Log In', self.browser.title)
+        # There we go, they enter the password right and can see the month view of the current month
+        self._login_attempt(self.test_username, self.test_password)
+        '''username_input, password_input, login_button = self._get_username_and_password_inputs()
+        username_input.send_keys(self.test_username)
+        password_input.send_keys(self.test_password)
+        login_button.click()'''
+        self.assertIn('Scheduler', self.browser.title)
+        # satisfied that her account works, she clicks the logout button
+        logout_button = self.browser.find_element_by_id('logout_button')
+        logout_button.click()
+        # This takes her to the log in page
+        self.assertIn('Log In', self.browser.title)
 
     def test_tiddlywinks_can_make_event(self):
         # Tiddlywinks is greeted by a log in screen
         self.assertIn('Log In', self.browser.title)
-        # They enter their username and password
-        username_input, password_input, login_button = self._get_username_and_password_inputs()
-        username_input.send_keys(self.test_username)
-        password_input.send_keys('assword123')
-        login_button.click()
-        # Oh no! They entered it wrong and got prompted to reenter the information
-        self.assertNotIn('Scheduler', self.browser.title)
-        # There we go, they enter the password right and can see the month view of the current month
-        username_input, password_input, login_button = self._get_username_and_password_inputs()
+        # They enter the right username and password and can see the month view of the current month
+        '''username_input, password_input, login_button = self._get_username_and_password_inputs()
         username_input.send_keys(self.test_username)
         password_input.send_keys(self.test_password)
-        login_button.click()
+        login_button.click()'''
+        self._login_attempt(self.test_username, self.test_password)
         self.assertIn('Scheduler', self.browser.title)
         # They click on the first day of the month and a form pops up
         day_one = self.browser.find_element_by_class_name('day_1')
