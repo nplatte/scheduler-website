44f203625973ed88642a6bce2eb27eb4019aaf58
nplatte <77999574+nplatte@users.noreply.github.com>
2022-01-31 10:03:20 -0600

    Added tests for model types

--- month_view/models.py
+++ month_view/models.py
@@ -1,5 +1,11 @@
+from datetime import timezone
 from django.db import models
+from datetime import date
 
 class Event(models.Model):
 
-    pass
+    title = models.CharField(max_length=20, default='')
+    description = models.CharField(max_length=100, blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
+    date = models.DateField(default=date.today())
+
---
+++ month_view/tests/test_models.py
@@ -0,0 +1,24 @@
+import datetime
+from turtle import title
+from month_view.models import Event
+from django.test import TestCase, LiveServerTestCase
+from datetime import time, date, datetime
+from django.utils import timezone
+
+class TestEvent(TestCase):
+
+    def setUp(self):
+        now = datetime.now()
+        self.new_event = Event.objects.create(title='test', description='test', time=now.strftime("%H:%M"))
+
+    def test_event_name_is_string(self):
+        self.assertIsInstance(self.new_event.title, str)
+
+    def test_event_description_is_string(self):
+        self.assertIsInstance(self.new_event.description, str)
+
+    def test_time_format(self):
+        self.assertIsInstance(self.new_event.time, str)
+
+    def test_date_format(self):
+        self.assertIsInstance(self.new_event.date, date)
