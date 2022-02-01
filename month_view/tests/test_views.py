5c50ee4a2daa204d4a3ca0aadb74093b4021156f
nplatte <77999574+nplatte@users.noreply.github.com>
2022-01-31 18:00:20 -0600

    removed and deleted old code so that login confirmed works as it's own app

--- month_view/tests/test_views.py
+++ month_view/tests/test_views.py
@@ -5,37 +5,6 @@ import month_view.models as models
 from django.contrib.auth.models import User
 from datetime import date, datetime
 
-'''class TestLoginPage(LiveServerTestCase):
-
-    the login page needs to be the first page people see
-    it needs to use the right template
-    redirect good users to month view
-    redirect bad users to login page
-
-    def setUp(self):
-        self.test_username = 'winkstiddly'
-        self.test_password = 'password123'
-        User.objects.create_user(username=self.test_username, email='test@test.com', password=self.test_password)
-
-    def tearDown(self):
-        pass
-
-    def test_login_uses_right_template(self):
-        response = self.client.get(reverse('login_page'))
-        self.assertTemplateUsed(response,'month_view/login.html')
-
-    def test_login_redirects_to_month_view_on_success(self):
-        response = self.client.post(reverse('login_page'), {'username': self.test_username, 'password': self.test_password}, follow=True)
-        self.assertRedirects(response, 'month_view/')
-
-    def test_login_redirects_to_login_on_fail(self):
-        response = self.client.post(reverse('login_page'), {'username': self.test_username, 'password': 'wrong_password'}, follow=True)
-        self.assertRedirects(response, reverse('login_page'))
-
-    def test_login_required_redirects_to_login_page(self):
-        response = self.client.get(reverse('month_page'), follow=True)
-        self.assertTemplateUsed(response, 'month_view/login.html')
-'''
 
 class TestMonthViewPage(LiveServerTestCase):
 
--- month_view/views.py
+++ month_view/views.py
@@ -1,25 +1,12 @@
-from datetime import datetime, date
+from datetime import datetime
 import calendar
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
-#from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .forms import EventForm
 from.models import Event
 
-'''def login_page(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('month_view/')
-        else:
-            return redirect('/')
-    return render(request, 'month_view/login.html')'''
 
 @login_required(login_url='/')
 def month_view_page(request):
@@ -33,7 +20,6 @@ def month_view_page(request):
         form=EventForm()
     month_events = {i: _get_events_on_day(i, month, year) for i in range(1, _get_days_in_month(month, year) + 1)}
     context = {
-        #'month_length': _get_days_in_month(month, year), 
         'form': form,
         'month_events': month_events
         }
