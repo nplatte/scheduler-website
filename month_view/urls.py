90d48bdc8e59436d7befc6ead10b14c9eaddb443
nplatte <77999574+nplatte@users.noreply.github.com>
2022-02-02 17:54:50 -0600

    converted to using include() in urls

--- month_view/urls.py
+++ month_view/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import month_view_page
 
-url_patterns = [
-    path('home/', month_view_page, name='month_page')
+urlpatterns = [
+    path('', month_view_page, name='month_page'),
 ]
