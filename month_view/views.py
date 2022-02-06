894583b0f5549240e8b44434f918928dfb55ebe0
nplatte <77999574+nplatte@users.noreply.github.com>
2022-02-05 20:01:58 -0600

    Added a function that returns the month name

--- month_view/views.py
+++ month_view/views.py
@@ -28,7 +28,8 @@ def month_view_page(request):
     month_events = {i: _get_events_on_day(i, month, year) for i in range(1, _get_days_in_month(month, year) + 1)}
     context = {
         'form': form,
-        'month_events': month_events
+        'month_events': month_events,
+        'month_name': _get_month_name(month)
         }
     return render(request, 'month_view/month_view.html', context)
 
@@ -38,3 +39,20 @@ def _get_days_in_month(month=datetime.now().month, year=datetime.now().year):
 
 def _get_events_on_day(day, month, year):
     return Event.objects.filter(date=f'{year}-{month}-{day}')
+
+def _get_month_name(month=datetime.now().month):
+    months = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December'
+    }
+    return months[month]
