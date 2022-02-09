2bbbf0cc9380eab1bcf80d22150ed1ed8ccb0228
nplatte <77999574+nplatte@users.noreply.github.com>
2022-02-09 16:33:29 -0600

    altered form tests to look for cleaner class and id names

--- month_view/tests/test_forms.py
+++ month_view/tests/test_forms.py
@@ -9,22 +9,24 @@ class TestNewEventForm(TestCase):
 
     def test_title_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('class="new_event_name"', form_html)
+        self.assertIn('class="event_title_input"', form_html)
+        self.assertIn('id="new_event_title"')
         self.assertIn('maxlength="20"', form_html)
         self.assertIn('required', form_html)
 
     def test_time_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('class="new_event_time"', form_html)
+        self.assertIn('class="event_time_input"', form_html)
 
     def test_date_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('class="new_event_date"', form_html)
+        self.assertIn('class="event_date_input"', form_html)
         self.assertIn('id="new_event_date"', form_html)
 
     def test_description_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('class="new_event_description"', form_html)
+        self.assertIn('class="event_description_input"', form_html)
+        self.assertIn('id="new_event_description"', form_html)
         self.assertIn('maxlength="100"', form_html)
 
 
@@ -35,21 +37,22 @@ class TestEditEventForm(TestCase):
 
     def test_title_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('id="edit_event_name"', form_html)
-        self.assertIn('class="edit_event_name"', form_html)
+        self.assertIn('class="event_title_input"', form_html)
+        self.assertIn('id="edit_event_title"', form_html)
         self.assertIn('maxlength="20"', form_html)
         self.assertIn('required', form_html)
 
     def test_time_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('class="edit_event_time"', form_html)
+        self.assertIn('class="event_time_input"', form_html)
 
     def test_date_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('class="edit_event_date"', form_html)
+        self.assertIn('class="event_date_input"', form_html)
         self.assertIn('id="edit_event_date"', form_html)
 
     def test_description_input_attributes(self):
         form_html = self.event_form.as_p()
-        self.assertIn('class="edit_event_description"', form_html)
+        self.assertIn('class="event_description_input"', form_html)
+        self.assertIn('id="edit_event_description"', form_html)
         self.assertIn('maxlength="100"', form_html)
