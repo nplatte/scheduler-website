df514d2bc81a9372e9b785df5c19fd336391db46
nplatte <77999574+nplatte@users.noreply.github.com>
2022-02-09 11:23:59 -0600

    Added a new more descriptive function name for turning the visibility on of the new event form

--- month_view/static/month_view/js/helper.js
+++ month_view/static/month_view/js/helper.js
@@ -1,4 +1,4 @@
-function ToggleForm(date) {
+function ToggleNewForm(date) {
     var form = document.getElementById('new_event_input');
     form.style.visibility = 'visible';
     var date_box = document.getElementById('new_event_date');
