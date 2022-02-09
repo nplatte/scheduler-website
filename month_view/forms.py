b607bac3480d0e686f444aede5dee75e077bab85
nplatte <77999574+nplatte@users.noreply.github.com>
2022-02-09 16:41:08 -0600

    Form uses expected class and id names

--- month_view/forms.py
+++ month_view/forms.py
@@ -22,8 +22,8 @@ class EditEventForm(ModelForm):
         model = Event
         fields = ['title', 'date', 'time', 'description']
         widgets = {
-            'title': forms.fields.TextInput(attrs={'class': 'edit_event_name', 'id': 'edit_event_name'}),
-            'time': forms.TimeInput(attrs={'class': 'edit_event_time'}),
-            'date': forms.DateInput(attrs={'class': 'edit_event_date', 'id': 'edit_event_date'}),
-            'description': forms.Textarea(attrs={'class': 'edit_event_description'})
+            'title': forms.fields.TextInput(attrs={'class': 'event_title_input', 'id': 'edit_event_title'}),
+            'time': forms.TimeInput(attrs={'class': 'event_time_input'}),
+            'date': forms.DateInput(attrs={'class': 'event_date_input', 'id': 'edit_event_date'}),
+            'description': forms.Textarea(attrs={'class': 'event_description_input', 'id': 'edit_event_description'})
             }
