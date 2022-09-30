d27b9dc13a567c1d4fe098bc81dad750aae0da3f
nplatte <77999574+nplatte@users.noreply.github.com>
2022-09-30 10:48:02 -0400

    removed new and edit events from old files

--- month_view/static/month_view/js/helper.js
+++ month_view/static/month_view/js/helper.js
@@ -1,31 +1,3 @@
-function ShowNewForm(date) {
-    var form = document.getElementById('new_event_input');
-    form.style.visibility = 'visible';
-    var date_box = document.getElementById('new_event_date');
-    date_box.value = date;
-}
-
-function ShowEditForm(title, date, id){
-    var form = document.getElementById('edit_event_input');
-    form.style.visibility = 'visible';
-    var title_box = document.getElementById('edit_event_title');
-    title_box.value = title;
-    var date_box = document.getElementById('edit_event_date');
-    date_box.value = date;   
-    var id_box = document.getElementById('event_id');
-    id_box.value = id;   
-}
-
-function HideNewForm() {
-    var form = document.getElementById('new_event_input');
-    form.style.visibility = 'hidden';
-}
-
-function HideEditForm() {
-    var form = document.getElementById('edit_event_input');
-    form.style.visibility = 'hidden';
-}
-
 function MouseOverDay(day, day_num) {
     day.style.backgroundColor = '#ADCAF7';
     var btn = document.getElementById('new_event_button_' + day_num);
