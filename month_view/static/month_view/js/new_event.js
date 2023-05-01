bca7ded759d3894a698b8f1c370afbb38d6d64da
nplatte <77999574+nplatte@users.noreply.github.com>
2023-05-01 18:13:50 -0400

    removed old code

--- month_view/static/month_view/js/new_event.js
+++ month_view/static/month_view/js/new_event.js
@@ -1,14 +1,3 @@
-function ShowNewForm(date) {
-    var form = document.getElementById('new_event_input');
-    form.style.visibility = 'visible';
-    var date_box = document.getElementById('new_event_date');
-    date_box.value = date;
-}
-
-function HideNewForm() {
-    var form = document.getElementById('new_event_input');
-    form.style.visibility = 'hidden';
-}
 
 function ToggleNewEventFormVisibility(date) {
     var form = document.getElementById('new_event_input');
