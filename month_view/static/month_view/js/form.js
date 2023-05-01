d40c3ccb952ad6ae23125cc841d459d45476cb3a
nplatte <77999574+nplatte@users.noreply.github.com>
2023-05-01 18:25:53 -0400

    Condensed javascript functions to one file

--- month_view/static/month_view/js/form.js
+++ month_view/static/month_view/js/form.js
@@ -1,3 +1,14 @@
+function ToggleNewEventFormVisibility(date) {
+    var form = document.getElementById('new_event_input');
+    if (form.style.visibility == 'visible') {
+        form.style.visibility = 'hidden';
+    } else {
+        form.style.visibility = 'visible';
+        var date_box = document.getElementById('new_event_date');
+        date_box.value = date;
+    }
+}
+
 function ShowEditForm(title, date, id){
     var form = document.getElementById('edit_event_input');
     form.style.visibility = 'visible';
