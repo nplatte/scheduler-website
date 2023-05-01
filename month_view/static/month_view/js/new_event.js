4f3f818eec2bae42043db1337a633806951d7474
nplatte <77999574+nplatte@users.noreply.github.com>
2023-05-01 17:25:44 -0400

    Added function that should toggle visibility on or off for the new event form

--- month_view/static/month_view/js/new_event.js
+++ month_view/static/month_view/js/new_event.js
@@ -9,3 +9,14 @@ function HideNewForm() {
     var form = document.getElementById('new_event_input');
     form.style.visibility = 'hidden';
 }
+
+function ToggleNewEventFormVisibility(date) {
+    var form = document.getElementById('new_event_input');
+    if (form.style.visibility = 'visible') {
+        form.style.visibility = 'hidden';
+    } else {
+        form.style.visibility = 'visible';
+        var date_box = document.getElementById('new_event_date');
+        date_box.value = date;
+    }
+}
