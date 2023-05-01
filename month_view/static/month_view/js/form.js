a77e0254ca0536bfdb55e5c4efdb0bb644f9fe27
nplatte <77999574+nplatte@users.noreply.github.com>
2023-05-01 18:36:20 -0400

    added toggle func for edit event

--- month_view/static/month_view/js/form.js
+++ month_view/static/month_view/js/form.js
@@ -9,6 +9,20 @@ function ToggleNewEventFormVisibility(date) {
     }
 }
 
+function ToggleEditEventFormVisibility(title, date, id) {
+    var form = document.getElementById('edit_event_input');
+    if (form.style.visibility == 'visible'){
+        form.style.visibility = 'hidden';
+    } else {
+        form.style.visibility = 'visible';
+        var title_box = document.getElementById('edit_event_title');
+        title_box.value = title;
+        var date_box = document.getElementById('edit_event_date');
+        date_box.value = date;   
+        var id_box = document.getElementById('event_id');
+        id_box.value = id;
+    }}
+
 function ShowEditForm(title, date, id){
     var form = document.getElementById('edit_event_input');
     form.style.visibility = 'visible';
