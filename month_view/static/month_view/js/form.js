8fa7974f0bfb30c77a02f660113148ff24df8f90
nplatte <77999574+nplatte@users.noreply.github.com>
2023-05-01 20:12:49 -0400

    removed old edit form show form and hide form functions

--- month_view/static/month_view/js/form.js
+++ month_view/static/month_view/js/form.js
@@ -22,19 +22,3 @@ function ToggleEditEventFormVisibility(title, date, id) {
         var id_box = document.getElementById('event_id');
         id_box.value = id;
     }}
-
-function ShowEditForm(title, date, id){
-    var form = document.getElementById('edit_event_input');
-    form.style.visibility = 'visible';
-    var title_box = document.getElementById('edit_event_title');
-    title_box.value = title;
-    var date_box = document.getElementById('edit_event_date');
-    date_box.value = date;   
-    var id_box = document.getElementById('event_id');
-    id_box.value = id;   
-}
-
-function HideEditForm() {
-    var form = document.getElementById('edit_event_input');
-    form.style.visibility = 'hidden';
-}
